09b01a8d60db52304c305328e864da9d9bbbb8be
smalllab0102 <smalllab0102@gmail.com>
2026-08-11 16:38:44 -0400

    Automatic save

--- Line_Limit.py
+++ Line_Limit.py
@@ -1,1 +1,10 @@
-l
+from collection import deque
+
+def keep_lines(file,max_lines = 1500000):
+    with open(file_dir, "r") as file:
+        kept_lines = deque(file, maxlen = max_lines)
+
+    with open(file_dir, "w") as file:
+        file.writelines(kept_lines)
+
+keep_lines("modbus_log", max_lines = 1500000)
