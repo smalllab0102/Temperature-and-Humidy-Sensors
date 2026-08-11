4a3684a96f8b43848227837ce7efe6181a8143e3
smalllab0102 <smalllab0102@gmail.com>
2026-08-11 17:01:42 -0400

    Automatic save

--- Line_Limit.py
+++ Line_Limit.py
@@ -3,10 +3,14 @@ from collections import deque
 def keep_lines(file,max_lines = 1500000):
     with open(file, "r") as f:
         first_line = f.readline()
+        rest_of_lines = f.readlines()
+        total_lines = len(rest_of_lines + 1)
+    
+    if total_line < max_lines:
         kept_lines = deque(file, maxlen = max_lines-1)
 
-    with open(file, "w") as f:
-        f.write(first_line)
-        f.writelines(kept_lines)
+        with open(file, "w") as f:
+            f.write(first_line)
+            f.writelines(kept_lines)
 
 keep_lines("modbus_log.csv", max_lines = 1500000)
