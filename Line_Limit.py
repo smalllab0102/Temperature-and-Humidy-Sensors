801ea911d2cc7208bb3de71cba4b49bf393da5e9
smalllab0102 <smalllab0102@gmail.com>
2026-08-11 17:09:08 -0400

    Automatic save

--- Line_Limit.py
+++ Line_Limit.py
@@ -4,11 +4,11 @@ def keep_lines(file,max_lines = 50):
     with open(file, "r") as f:
         first_line = f.readline()
         rest_of_lines = f.readlines()
-        total_lines = len(rest_of_lines + 1)
+        total_lines = len(rest_of_lines) + 1
         f.seek(0)
         first_line = f.readline()
     
-    if total_line < max_lines:
+    if total_lines > max_lines:
         kept_lines = deque(file, maxlen = max_lines-1)
 
         with open(file, "w") as f:
