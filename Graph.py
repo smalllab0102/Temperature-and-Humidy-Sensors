1b38319c35f37b27d68b2d54356dab6ecef97f9c
smalllab0102 <smalllab0102@gmail.com>
2026-08-12 12:26:55 -0400

    Automatic save

--- Graph.py
+++ Graph.py
@@ -45,7 +45,6 @@ def update(frame):
         ax.autoscale_view()
     fig.canvas.draw_idle()
     counter += 1
-    print(counter)
     if counter >= 10:
         plt.savefig("Temp and Humidity Graph.png")
         counter = 0
