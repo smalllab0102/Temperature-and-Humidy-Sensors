f85f676189925551dce519c399c19eb4dc72ebb2
smalllab0102 <smalllab0102@gmail.com>
2026-08-12 10:05:42 -0400

    Automatic save

--- Graph.py
+++ Graph.py
@@ -3,8 +3,11 @@ import random
 from matplotlib.animation import FuncAnimation
 import pandas as pd
 
-fig, ax = plt.subplots()
-graph, = ax.plot([],[],color = 'b')
+fig, (ax1, ax2) = plt.subplots(2,1)
+graph1, = ax1.plot([],[],color = 'b')
+graph2, = ax2.plot([],[],color = 'b')
+graph = [graph1,graph2]
+axes = [ax1, ax2]
 plt.ylim(0,30)
 x = [1]
 
@@ -12,17 +15,21 @@ def update(frame):
     global graph
 
     data = pd.read_csv('modbus_log.csv')
-    y = data['Temp C']
-    x = list(range(1, len(y) + 1))
+    y = data[['Temp C'],['Humidity']]
+    x = list(range(1, len(data) + 1))
     # creating a new graph or updating the graph
-    graph.set_xdata(x)
-    graph.set_ydata(y)
-    ax.relim()
-    ax.autoscale_view()
+    for axes, graph, data in zip(axes, graph, y):
+        graph.set_xdata(x)
+        graph.set_ydata(y)
+        ax.relim()
+        ax.autoscale_view()
     fig.canvas.draw_idle()
-    plt.title("Temperature vs Time")
-    plt.xlabel("Time")
-    plt.ylabel("Temperature (celsius)")
+    ax1.title("Temperature vs Time")
+    ax2.title("Humidity vs Time")
+    ax1.xlabel("Time")
+    ax2.xlabel("Time")
+    ax1.set_ylabel("Temperature (celsius)")
+    ax2.set_ylabel("Humidity")
     return graph,
 
 anim = FuncAnimation(fig, update, interval=1000)
